2ed6c2607b9af6e6db28050d623b03081bc6a781
Enes Korac <korac.enes2@gmail.com>
2025-03-18 21:46:12 +0100

    created ui for profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,9 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-gray-50 ${geistSans.variable} ${geistMono.variable} antialiased `}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
