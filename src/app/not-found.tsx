7bb79656f5d3e4ed8aae059efcca5a134c79187d
Emin Kocan <kocanmn.dev@gmail.com>
2025-04-14 10:25:10 +0200

    fix: fix build errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="mx-auto max-w-md text-center">
         <h1 className="text-9xl font-extrabold tracking-tighter text-gray-900">404</h1>
-        <p className="mt-4 text-lg text-gray-500">The page you're looking for doesn't exist or has been moved.</p>
+        <p className="mt-4 text-lg text-gray-500">The page you are looking for doesn not exist or has been moved.</p>
         <div className="mt-8">
           <Button asChild variant="outline" className="gap-2">
             <Link href="/">
