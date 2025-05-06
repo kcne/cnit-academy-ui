5f446ade49c231c6f166675e5c89ba0e4d33aaa6
mehmedmekic08 <mekicmehmed2008@gmail.com>
2025-05-06 20:41:44 +0200

    page refactored and fully responsive

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -3,9 +3,10 @@
 import { Button } from "@/components/ui/button"
 import { useState, useEffect } from "react";
 import axios from "axios"
-import { Clock, Coins, UsersRound } from "lucide-react";
+import { CheckCheck, ChevronDown, Clock, Coins, Timer, UsersRound } from "lucide-react";
 import Link from "next/link";
 import { BASE_URL } from "@/app/providers/userContext";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
 
 
 export default function Courses({ params: { id } }: any){
@@ -21,63 +22,73 @@ export default function Courses({ params: { id } }: any){
 
     return(
         <div>
-            <div className="flex flex-wrap p-16 justify-center gap-x-20 justify-items-center ">
+            <div className="flex flex-wrap p-16 justify-center justify-items-center ">
 
     {/*Course description div*/}
 
-                <div className="bg-sky-100 mb-16 h-max rounded-2xl min-w-80 w-3/5 p-5 shadow-2xl flex justify-items-start" >
-                    <div className="">
+                <div className="h-max rounded-xl border-2 border-gray-200 min-w-80 w-full p-5 flex justify-items-start pb-40" >
+                    <div className="w-full">
                         <div className="pt-4 ">
                             <h1 className="text-4xl font-bold">{data?.title}</h1>
+                            <h1 className="py-2">Course created: {date.getDate()}/{date.getMonth()}/{date.getFullYear()}</h1>
+                            {/* Information div*/}
+                            <div className="flex h-full w-full font-semibold justify-center md:justify-start">
+                                <div className="flex flex-wrap relative w-3/4 md:w-1/4 justify-between py-5">
+                                <h1 className="px-5 py-2 md:px-0 flex"><Clock color="#000000" />{Math.round(data?.durationInHours * 10)/10} Hours</h1>
+                                <h1 className="px-5 py-2 flex"><Coins color="#000000" />{data?.coins} Coins</h1>
+                                <h1 className="px-5 py-2 flex"><UsersRound color="#000000" />{data?.studentCount} Students</h1>
+                                </div>
+                            </div>
                             <h2 className="text-lg w-full relative h-max font-semibold">{data?.description}</h2>
+
+                        
                         </div>
-                        <div className="py-4 flex items-center">
-                            <Button variant="outline" className="border-blue-600 border-2 rounded-lg text-blue-600 bg-sky-100" onClick={()=>{
+                        <div className="py-4 flex justify-center md:justify-start">
+                            <Button variant="outline" className="border-black border-2 rounded-lg text-white bg-black hover:" onClick={()=>{
                                 axios.put(BASE_URL + "/api/course/" + id + "/start",{}, {headers: {Authorization: `Bearer ${token}`}});
                             }}>Start</Button>
-                            <h1 className="px-4">Course created: {date.getFullYear()}/{date.getMonth()}/{date.getDate()}</h1>
                         </div>
+                        
+                            {/* lectures div */}
+                            <div className="flex">
+                                <h1 className="font-medium text-black text-3xl flex">Lectures</h1>
+                            </div>
+                                    {data?.lectures.map((lecture:any) => (
+                                        <Accordion type="single" collapsible>
+                                            <AccordionItem value="1">
+                                                <div className="pt-3 flex flex-col">
+                                                        <AccordionTrigger className="flex justify-start">
+                                                            <div className="font-bold text-xl">
+                                                                    {lecture.title}
+                                                            </div>
+                                                            <ChevronDown></ChevronDown>
+                                                        </AccordionTrigger>
+                                                        <AccordionContent>
+                                                        <div className="flex flex-col md:flex-row justify-between items-center">
+                                                                <div className="p-5">
+                                                                    {lecture.content}
+                                                                </div>
+
+                                                                <div className="flex items-center ">
+                                                                {lecture?.started ? lecture?.finished ?  <CheckCheck /> :  <Timer /> : "" }
+                                                                    <Button variant="outline" className="border-black border-2 rounded-lg text-black m-2"><Link href={"/lecture/"+ lecture.id}>Go to lecture</Link></Button>
+                                                                </div>
+                                                            </div>
+                                                        </AccordionContent>
+                                                </div>
+                                            </AccordionItem>
+                                        </Accordion>
+                                    ))}
+
                     </div>
                 </div>
 
-    {/*Information div*/}
-
-                <div className="flex h-full w-max font-semibold ">
-                    <div className=" rounded-2xl shadow-2xl flex flex-wrap relative">
-                    <h1 className="py-10 px-5 w-1/2 flex"><Clock color="#000000" />{Math.round(data?.durationInHours * 10)/10} Hours</h1>
-                    <h1 className="py-10 px-5 w-1/2 flex"><Coins color="#000000" />{data?.coins} Coins</h1>
-                    <h1 className="py-10 px-5 w-1/2 flex"><UsersRound color="#000000" />{data?.studentCount} Students</h1>
-                    </div>
-                </div>
+    
             </div>
 
-    {/*Lectures div*/}
-
-            <div className="font-semibold min-w-80 flex justify-self-center w-3/4 ">
-                    <div className=" w-full h-full p-2 shadow-xl rounded-2xl">
-                        <h1 className="text-2xl font-bold">Lectures</h1>
-                        <div className="">
-                        {data?.lectures.map((lecture:any) => (
-                            <div className="pt-5">
-                                <div className="flex justify-between">
-                                    <div>
-                                        {lecture.title}
-                                    </div>
-                                    <Button variant="outline" className="border-blue-600 border-2 rounded-lg text-blue-600"><Link href={"/lecture/"+ lecture.id}>Go to lecture</Link></Button>
-                                </div>
-                                <div className="w-full my-5"> 
-                                    <h1 className={lecture?.started ? lecture?.finished ? 
-                                        "bg-orange-400 rounded-lg text-center" : 
-                                        "bg-orange-300 rounded-lg text-center" : 
-                                        "bg-orange-200 rounded-lg text-center"}>
-                                        
-                                        {lecture?.started ? lecture?.finished ? "Finished" : "Unfinished" : "Haven't started" }
-                                    </h1>
-                                </div>
-                            </div>))}
-                        </div>
-                    </div>
-                </div>
+    {/* Lectures div
+            
+           */}
         </div>
 
     )
