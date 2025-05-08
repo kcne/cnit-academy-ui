113b580f6bfe59d662d190e5996e5efd53a9cc25
segatcm <segatheycallme@proton.me>
2025-05-08 19:55:22 +0200

    fix .env secrets leak

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function LoginPage() {
   return (
-    <GoogleOAuthProvider clientId="949948282254-510vhq0l1ouh63ussgb5af1ne0uc07lr.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={process.env.OAUTH2_CLIENT_ID ?? ""}>
       <Form />
     </GoogleOAuthProvider>
   );
